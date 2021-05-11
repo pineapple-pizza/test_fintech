460878a2800808775e6d0016a0b3a0d6c2f1ae36
an.aslanian <an.aslanian@gmail.com>
2021-05-11 21:13:49 +0200

    fix

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app align="center">
+    <v-content>
+      <ranking>
+      </ranking>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Ranking from '@/components/ranking';
+
+export default {
+  name: 'App',
+  components: {
+    'ranking': Ranking
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
